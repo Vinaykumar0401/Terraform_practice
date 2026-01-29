aae2f28e946ffdb219fd547ed8cd2a4d64b17aa8
vinay.reddy <kvinay80130@gmail.com>
2026-01-29 13:02:42 +0530

    ec2-terraform-output-variable

--- day01/ec2-terraform-project/variable.tf
+++ day01/ec2-terraform-project/variable.tf
@@ -13,3 +13,12 @@ variable "key_name" {
   default     = "linux"
   description = "The name of the key pair to use for the instance"
 }
+
+output "instance_id" {
+  value       = aws_instance.terraform_instance.id
+  description = "The ID of the EC2 instance"
+}
+output "instance_public_ip" {
+  value       = aws_instance.terraform_instance.public_ip
+  description = "The public IP address of the EC2 instance"
+}
